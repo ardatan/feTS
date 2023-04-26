3b4ae2adb0ef53434327421c02ce5e605d883bd7
beerose <alexsandra.sikora@gmail.com>
2023-04-26 14:53:56 +0200

    unused var fix

--- examples/trpc-openapi/src/pages/api/openapi.json.ts
+++ examples/trpc-openapi/src/pages/api/openapi.json.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { openApiDocument } from '../../server/openapi';
 
 // Respond with our OpenAPI schema
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).send(openApiDocument);
 };
 
