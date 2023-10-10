0d289f6c2934983af22c757b6b5438170877e59b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-10 21:12:49 +0200

    nextra

---
+++ website/src/pages/server/integrations/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  uwebsockets: 'µWebSockets',
+  'aws-lambda': 'AWS Lambda',
+  'azure-functions': 'Azure Functions',
+  'cloudflare-workers': 'Cloudflare Workers',
+  gcp: 'Google Cloud Functions',
+  nextjs: 'Next.js',
+  bun: 'Bun',
+  deno: 'Deno',
+  'node-http': 'Node:HTTP',
+};
