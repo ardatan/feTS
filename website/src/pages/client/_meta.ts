e612b7821803289ee82a8d74fc46f181477cb808
Dimitri POSTOLOV <en3m@ya.ru>
2023-10-10 21:56:15 +0200

    FeTS x Nextra 3 (#651)

---
+++ website/src/pages/client/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  'quick-start': 'Quickstart',
+  'error-handling': 'Error Handling',
+  'request-params': 'Request Parameters',
+  plugins: 'Plugins',
+};
---
+++ website/src/pages/server/_meta.ts
@@ -0,0 +1,14 @@
+export default {
+  'quick-start': 'Quickstart',
+  'type-safety-and-validation': 'Type-Safety & Validation',
+  middlewares: 'Middlewares',
+  'error-handling': 'Error Handling',
+  zod: 'Programmatic Schemas with Zod',
+  testing: 'Testing',
+  cors: 'CORS',
+  cookies: 'Cookies',
+  plugins: 'Plugins',
+  openapi: 'OpenAPI (Swagger)',
+  comparison: 'Comparison',
+  integrations: 'Integration & Deployment',
+};
