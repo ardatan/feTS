4808a1dba251596be13d082460310d6c7e4ed523
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-10-21 00:12:46 +0300

    add nextjs app directory example (#507)
    Co-authored-by: Aleksandra <alexsandra.sikora@gmail.com>

---
+++ examples/nextjs-app-dir/README.md
@@ -0,0 +1,17 @@
+This is a [Next.js](https://nextjs.org/) project bootstrapped with
+[`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, clone this project, install the dependencies and run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
+
+Open [http://localhost:3000/api/greetings](http://localhost:3000/api/greetings) with your browser to
+see the JSON response.
---
+++ examples/nextjs-app-dir/src/app/api/[...slug]/route.ts
@@ -0,0 +1,33 @@
+import { createRouter, Response } from 'fets';
+
+const handler = createRouter({
+  swaggerUI: {
+    endpoint: '/api/docs',
+  },
+  openAPI: {
+    endpoint: '/api/openapi.json',
+  },
+  fetchAPI: {
+    Response: globalThis.Response,
+  },
+}).route({
+  method: 'GET',
+  path: '/api/greetings',
+  schemas: {
+    responses: {
+      200: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'string',
+          },
+        },
+        required: ['message'],
+        additionalProperties: false,
+      },
+    },
+  } as const,
+  handler: () => Response.json({ message: 'Hello World!' }),
+});
+
+export { handler as GET, handler as POST };
