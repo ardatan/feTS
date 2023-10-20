01941ab055f10b6c9222d25a60e38d32b2150f2e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-10-20 13:03:34 -0500

    :facepalm:

--- packages/fets/src/plugins/typebox.ts
+++ packages/fets/src/plugins/typebox.ts
@@ -117,6 +117,7 @@ export function useTypeBox({
                   },
                 );
               }
+              return jsonObj;
             }),
           configurable: true,
         });
