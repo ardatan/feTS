9d651dd8dbc4bd72ccd013b79c8cf8e192fcdac8
beerose <alexsandra.sikora@gmail.com>
2023-10-20 15:59:58 -0500

    try formatting again

--- examples/nextjs-app-dir/README.md
+++ examples/nextjs-app-dir/README.md
@@ -1,4 +1,5 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a [Next.js](https://nextjs.org/) project bootstrapped with
+[`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
 
@@ -12,4 +13,5 @@ yarn dev
 pnpm dev
 ```
 
-Open [http://localhost:3000/api/greetings](http://localhost:3000/api/greetings) with your browser to see the JSON response.
+Open [http://localhost:3000/api/greetings](http://localhost:3000/api/greetings) with your browser to
+see the JSON response.
--- examples/nextjs-app-dir/src/app/page.tsx
+++ examples/nextjs-app-dir/src/app/page.tsx
@@ -1,5 +1,5 @@
-import styles from "./page.module.css";
-import Image from "next/image";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -85,9 +85,7 @@ export default function Home() {
           <h2>
             Deploy <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
         </a>
       </div>
     </main>
