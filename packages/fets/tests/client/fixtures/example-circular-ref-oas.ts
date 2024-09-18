86a5a2ea32cf942179ff8071ac7e80f8058c32a9
Ahmet Çelik <ahm3tcelik72@gmail.com>
2024-09-18 03:44:00 +0300

    Update example-circular-ref-oas.ts

--- packages/fets/tests/client/fixtures/example-circular-ref-oas.ts
+++ packages/fets/tests/client/fixtures/example-circular-ref-oas.ts
@@ -30,48 +30,48 @@ export default {
         },
       },
     },
-    "/tree-list": {
-			get: {
-				tags: ["tree-list"],
-				summary: "Get tree list",
-				description: "",
-				operationId: "getTreeList",
-				responses: {
-					"200": {
-						description: "successful operation",
-						content: {
-							"application/json": {
-								schema: {
-									$ref: "#/components/schemas/TreeNode",
-								},
-							},
-						},
-					},
-					"404": {
-						description: "Tree not found",
-					},
-				},
-			},
-		},
+    '/tree-list': {
+      get: {
+        tags: ['tree-list'],
+        summary: 'Get tree list',
+        description: '',
+        operationId: 'getTreeList',
+        responses: {
+          '200': {
+            description: 'successful operation',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/TreeNode',
+                },
+              },
+            },
+          },
+          '404': {
+            description: 'Tree not found',
+          },
+        },
+      },
+    },
   },
   components: {
     schemas: {
-			TreeNode: {
-				type: "object",
-				properties: {
-					number: {
+      TreeNode: {
+        type: 'object',
+        properties: {
+          number: {
             type: 'integer',
             format: 'int64',
             example: 10,
-					},
-					children: {
-						type: "array",
-						items: {
-							$ref: "#/components/schemas/TreeNode",
-						},
-					},
-				},
-			},
+          },
+          children: {
+            type: 'array',
+            items: {
+              $ref: '#/components/schemas/TreeNode',
+            },
+          },
+        },
+      },
       Node: {
         type: 'object',
         properties: {
