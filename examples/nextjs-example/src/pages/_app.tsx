47b41858182cd131843f8350c1a5e6a82025a4ec
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-04-02 14:51:42 +0300

    Update _app.tsx

--- examples/nextjs-example/src/pages/_app.tsx
+++ examples/nextjs-example/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-// @ts-expect-error - CSS types are missing
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
